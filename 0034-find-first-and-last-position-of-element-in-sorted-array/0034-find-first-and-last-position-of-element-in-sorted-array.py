6a03f03b106ad502301d9eca38ca81e1d31a9868
teomz <109972883+teomz@users.noreply.github.com>
2025-10-02 16:47:17 +0800

    Time: 0 ms (100%), Space: 18.9 MB (93.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,30 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left,right = 0, len(nums) -1
+        def findLeft(nums, target):
+            left, right = 0, len(nums) - 1
+            idx = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                if nums[mid] == target:
+                    idx = mid
+            return idx
 
-        while left <= right:
-            mid = (left + right)//2
-            print(left,right,mid)
-            
-            # start with a binary search to find the possible position of target
-            if nums[mid] > target:  
-                right = mid - 1
+        def findRight(nums, target):
+            left, right = 0, len(nums) - 1
+            idx = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                if nums[mid] == target:
+                    idx = mid
+            return idx
 
-            # if found, try to find the left most and right most position 
-            elif nums[mid] == target:
-                left,right = mid , mid
-                while left > 0 and nums[left-1] == target:
-                    left = left - 1
-                while right < len(nums) -1 and nums[right+1] == target:
-                    right = right + 1
-                
-                return [left,right]
+        return [findLeft(nums, target), findRight(nums, target)]
 
-            # else continue with binary search
-            else:
-                left = mid + 1
-                
-
-        return [-1,-1]
-            
         
