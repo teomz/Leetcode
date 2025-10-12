95380728f1ea2afe25fe4fdd238cbe8a12b071f8
teomz <109972883+teomz@users.noreply.github.com>
2025-10-13 01:06:48 +0800

    Time: 1923 ms (50.17%), Space: 18.3 MB (7.14%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,20 +3,21 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows = [[] for _ in range(9)]
-        columns = [[] for _ in range(9)]
-        boxes = [[] for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        columns = [set()  for _ in range(9)]
+        boxes = [set()  for _ in range(9)]
 
         empty_list = []
 
         for i in range(9):
             for j in range(9):
-                if board[i][j] == '.':
+                val = board[i][j]
+                if val == '.':
                     empty_list.append(i*9 + j)
                 else:
-                    columns[j].append(board[i][j])
-                    rows[i].append(board[i][j])
-                    boxes[(i //3)*3 + j//3].append(board[i][j])
+                    columns[j].add(val)
+                    rows[i].add(val)
+                    boxes[(i //3)*3 + j//3].add(val)
         
 
         
@@ -33,9 +34,9 @@ class Solution:
             for num in range(1,10):
                 num = str(num)
                 if num not in columns[column_index] and num not in rows[row_index] and num not in boxes[box_index]:
-                    columns[column_index].append(num)
-                    rows[row_index].append(num)
-                    boxes[box_index].append(num)
+                    columns[column_index].add(num)
+                    rows[row_index].add(num)
+                    boxes[box_index].add(num)
                     board[row_index][column_index] = num
 
 
@@ -43,9 +44,9 @@ class Solution:
                         return True
 
                     else:
-                        columns[column_index].pop()
-                        rows[row_index].pop()
-                        boxes[box_index].pop()
+                        columns[column_index].remove(num)
+                        rows[row_index].remove(num)
+                        boxes[box_index].remove(num)
 
             return False 
 
