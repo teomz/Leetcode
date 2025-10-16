e1999cd9876cd3859d913b5064b40cd52d5b3791
teomz <109972883+teomz@users.noreply.github.com>
2025-10-16 21:59:09 +0800

    Time: 5 ms (2.84%), Space: 18.2 MB (16.5%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         output = []
-        output.append(nums)
+
         def backtrack(combi):
 
             if len(combi) == len(nums):
-                if combi not in output:
-                    output.append(combi[:])
+                output.append(combi[:])
 
-            for i in range(len(nums)):
-                if nums[i] not in combi:
-                    combi.append(nums[i])
-                    backtrack(combi)
-                    combi.pop()
+            for num in nums:
+                if num not in combi:
+                    backtrack(combi + [num])
+
                 
         
         backtrack([])
