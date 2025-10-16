727e96148ccbca75544408ef1c63f9c98c86f823
teomz <109972883+teomz@users.noreply.github.com>
2025-10-17 00:50:48 +0800

    Time: 505 ms (95.74%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,7 +1,8 @@
-DELETE p1
-FROM person p1
-JOIN person p2
-  ON p1.email = p2.email
- AND p1.id > p2.id;
-
-
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id FROM (
+        SELECT MIN(id) AS id
+        FROM Person
+        GROUP BY email
+    ) AS t
+);
