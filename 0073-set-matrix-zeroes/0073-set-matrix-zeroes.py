7cb60e31391225455570445a7f3d487d7745530b
teomz <109972883+teomz@users.noreply.github.com>
2025-11-19 12:02:14 +0800

    Time: 3 ms (71.54%), Space: 18.6 MB (37.03%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,16 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        changed = [[True if i == 0 else False for i in j] for j in matrix]
+        zero_rows, zero_cols = (
+            {j for i in range(len(matrix[0])) for j in range(len(matrix)) if matrix[j][i] == 0},
+            {i for i in range(len(matrix[0])) for j in range(len(matrix)) if matrix[j][i] == 0}
+        )
 
         for i in range(len(matrix[0])):
             for j in range(len(matrix)):
-                val = matrix[j][i]
-                if max(changed[j]) or  max([boolean[i] for boolean in changed]) :
+                if j in zero_rows or i in zero_cols:
                     matrix[j][i] = 0
 
-        
         return matrix
+        
 
 
         
