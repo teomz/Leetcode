0d41de6679186d1d2d1987126d644759cd9d5c34
teomz <109972883+teomz@users.noreply.github.com>
2025-12-07 19:12:37 +0800

    Time: 12 ms (5.16%), Space: 18 MB (44.53%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        output = []
+        temp = []
+        def backtrack(l,r,temp):
+            if l == 0 and r == 0:
+                output.append(''.join(temp[:]))
+            
+            if l > 0:
+                print(temp)
+                temp.append('(')
+                backtrack(l-1,r,temp)
+                temp.pop()
+
+            if l < r:
+                print(temp)
+                temp.append(')')
+                backtrack(l,r-1,temp)
+                temp.pop()
+
+            
+        backtrack(n,n,temp)
+
+        return output
+
+
+        
