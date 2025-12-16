2ba6579b9e761284b4395d6137fe719fe6e4a857
teomz <109972883+teomz@users.noreply.github.com>
2025-12-16 16:45:32 +0800

    Time: 287 ms (5.01%), Space: 18.3 MB (56.56%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -2,12 +2,12 @@ class Solution:
     def __init__(self):
         self.mp = {}
     def isScramble(self, s1: str, s2: str) -> bool:
-        n = len(s1)
+        m = len(s1)
 
         if s1 == s2:
             return True
 
-        if n == 1:
+        if s1 == 1:
             return False
 
         key = s1 + ' ' + s2
@@ -15,20 +15,15 @@ class Solution:
         if key in self.mp:
             return self.mp[key]
 
-        for i in range(1,n):
-            if self.isScramble(s1[:i],s2[:i]) and self.isScramble(s1[i:],s2[i:]):
-                self.mp[key] = True
-                return True
-            if self.isScramble(s1[:i],s2[n-i:]) and self.isScramble(s1[i:],s2[:n-i]):
+        for i in range(1,m):
+            if self.isScramble(s1[i:],s2[i:]) and self.isScramble(s1[:i],s2[:i]):
                 self.mp[key] = True
                 return True
 
-        
+            if self.isScramble(s1[i:],s2[:m-i]) and self.isScramble(s1[:i],s2[m-i:]):
+                self.mp[key] = True
+                return True
+
         self.mp[key] = False
         return False
-
-
-        
-        
-
         
