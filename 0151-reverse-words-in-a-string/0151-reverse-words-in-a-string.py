96ce8173de26d684e20cdf07f122fa88c6b7f46f
teomz <109972883+teomz@users.noreply.github.com>
2026-03-05 22:13:53 +0800

    Time: 1 ms (41.41%), Space: 19.4 MB (75.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -3,11 +3,9 @@ class Solution:
 
         string = []
 
-        word_list = s.split(' ')
+        word_list = s.split()
         print(word_list)
         for i in range(len(word_list)-1,-1,-1):
-            print(i)
-            if word_list[i] != "":
-                string.append(word_list[i])
+            string.append(word_list[i])
 
         return " ".join(string)
